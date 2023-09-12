82408621baa3d079fb9a5ec716d4c10da1524769
emicab <emicabanillas.cab@gmail.com>
2023-09-11 21:57:23 -0300

    cambio de modelo

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -8,19 +8,22 @@ const Model = ({colorBtn, setLoad}) => {
     const {scene, nodes} = useGLTF("./capa_model.glb");
     const model = useRef()
 
+    console.log(nodes.botones)
+
     nodes.Capa.material.color = new THREE.Color(colorBtn);
+    nodes.botones.material.color = new THREE.Color('#FFF');
     nodes.Capa.material.roughness = 0.7;
     nodes.Capa.material.metalness = 0.2;
 
   /*   console.log(nodes) */
     useFrame((state, delta) => {
         const time = state.clock.getElapsedTime()
-        model.current.rotation.y = Math.sin(time) * 0.5
+        model.current.rotation.y = Math.sin(time) * 0.2
         
     })
     setLoad(true)
     return <Suspense>
-        <primitive ref={model} object={scene} position-y={-2.7} />;
+        <primitive ref={model} object={scene} position-y={-2.7} scale={0.43}/>;
     </Suspense>
 };
 
